dbfb7543560a3d64a3088dcc1901af02ce283e84
Rajeshwar Ojha <rajeshwar.ee17@iitp.ac.in>
2023-12-08 01:15:48 +0530

    Basic project structure setup

--- src/main/java/com/example/DesignPatterns.java
+++ src/main/java/com/example/DesignPatterns.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesignPatterns {
+
     public static void main(String[] args) {
         SpringApplication.run(DesignPatterns.class, args);
     }
